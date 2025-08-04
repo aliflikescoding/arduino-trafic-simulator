9f7b0a70ca736f21efe2c14ac264ad6cd441a0b5
Alif Wibowo <alif.likes.coding@gmail.com>
2025-08-04 20:05:25 +0700

    move images to root

--- trafic-simulator/src/main.cpp
+++ trafic-simulator/src/main.cpp
@@ -3,7 +3,6 @@
 // variable declarations
 
 const int buttonPinRight = 2;
-const int buttonPinLeft = 13;
 
 const int leftRedLed = 12;
 const int leftYellowLed = 11;
@@ -22,8 +21,6 @@ void setup()
 {
   // Buttons as input (use INPUT_PULLUP if wired to ground)
   pinMode(buttonPinRight, INPUT);
-  pinMode(buttonPinLeft, INPUT);
-
   // Left side LEDs as output
   pinMode(leftRedLed, OUTPUT);
   pinMode(leftYellowLed, OUTPUT);
@@ -45,42 +42,36 @@ void loop()
 
 // put function definitions here:
 void startTrafficPhases() {
-  // Phase 1: Left Green, Right Red
-  digitalWrite(leftGreenLed, HIGH); // left green on
-  digitalWrite(leftYellowLed, LOW); // left yellow off
-  digitalWrite(leftRedLed, LOW);    // left red off
+  digitalWrite(leftGreenLed, HIGH);
+  digitalWrite(leftYellowLed, LOW);
+  digitalWrite(leftRedLed, LOW);
 
-  digitalWrite(rightRedLed, HIGH);   // right red on
-  digitalWrite(rightYellowLed, LOW); // right yellow off
-  digitalWrite(rightGreenLed, LOW);  // right green off
-  delay(5000);                       // 5 seconds next step
+  digitalWrite(rightRedLed, HIGH);
+  digitalWrite(rightYellowLed, LOW);
+  digitalWrite(rightGreenLed, LOW);
+  delay(5000);
 
-  // Phase 2: Left Yellow, Right Red
-  digitalWrite(leftGreenLed, LOW);   // left green off`
-  digitalWrite(leftYellowLed, HIGH); // left yellow on
-  digitalWrite(leftRedLed, LOW);     // left red off
+  digitalWrite(leftGreenLed, LOW);
+  digitalWrite(leftYellowLed, HIGH);
+  digitalWrite(leftRedLed, LOW);
   delay(2000);
-  digitalWrite(leftYellowLed, LOW); // left yellow off
+  digitalWrite(leftYellowLed, LOW);
 
-  // Phase 3: Left Red, Right Yellow
   digitalWrite(leftRedLed, HIGH);
   digitalWrite(rightRedLed, LOW);
   digitalWrite(rightYellowLed, HIGH);
   delay(1500);
   digitalWrite(rightYellowLed, LOW);
 
-  // Phase 4: Left Red, Right Green
   digitalWrite(rightGreenLed, HIGH);
   delay(5000);
 
-  // Phase 5: Left Red, Right Yellow
-  digitalWrite(rightGreenLed, LOW);   // right green off`
-  digitalWrite(rightYellowLed, HIGH); // right yellow on
-  digitalWrite(rightRedLed, LOW);     // right red off
+  digitalWrite(rightGreenLed, LOW);
+  digitalWrite(rightYellowLed, HIGH);
+  digitalWrite(rightRedLed, LOW);
   delay(2000);
   digitalWrite(rightYellowLed, LOW);
 
-  // Phase 6:  Left Yellow, Right Red
   digitalWrite(leftRedLed, LOW);
   digitalWrite(rightRedLed, HIGH);
   digitalWrite(leftYellowLed, HIGH);
